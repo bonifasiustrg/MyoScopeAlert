d970fd3011ee60c00e86c048559ce73cbb2a26ee
Bonifasius Tarigan <bonifasius.tambak@gmail.com>
2023-09-12 10:26:08 +0700

    add some component for profile screen

--- app/src/main/java/com/apicta/myoscopealert/data/user/ProfileResponse.kt
+++ app/src/main/java/com/apicta/myoscopealert/data/user/ProfileResponse.kt
@@ -1,22 +1,89 @@
 package com.apicta.myoscopealert.data.user
 
+
+import com.google.gson.annotations.SerializedName
+
 data class ProfileResponse(
+    @SerializedName("data")
     val `data`: Data?,
+    @SerializedName("error")
     val error: Any?,
+    @SerializedName("message")
     val message: String?,
+    @SerializedName("status")
     val status: String?
 ) {
     data class Data(
-        val profile: Any?,
+        @SerializedName("profile")
+        val profile: Profile?,
+        @SerializedName("user")
         val user: User?
     ) {
-        data class User(
-            val created_at: String?,
-            val email: String?,
-            val email_verified_at: Any?,
+        data class Profile(
+            @SerializedName("address")
+            val address: String?,
+            @SerializedName("age")
+            val age: String?,
+            @SerializedName("condition")
+            val condition: String?,
+            @SerializedName("created_at")
+            val createdAt: String?,
+            @SerializedName("device_id")
+            val deviceId: String?,
+            @SerializedName("emergency_phone")
+            val emergencyPhone: String?,
+            @SerializedName("fullname")
+            val fullname: String?,
+            @SerializedName("gender")
+            val gender: String?,
+            @SerializedName("id")
             val id: String?,
+            @SerializedName("phone")
+            val phone: String?,
+            @SerializedName("relations")
+            val relations: List<Relation?>?,
+            @SerializedName("updated_at")
+            val updatedAt: String?,
+            @SerializedName("user_id")
+            val userId: String?
+        ) {
+            data class Relation(
+                @SerializedName("address")
+                val address: String?,
+                @SerializedName("age")
+                val age: String?,
+                @SerializedName("created_at")
+                val createdAt: String?,
+                @SerializedName("emergency_phone")
+                val emergencyPhone: String?,
+                @SerializedName("fullname")
+                val fullname: String?,
+                @SerializedName("gender")
+                val gender: String?,
+                @SerializedName("id")
+                val id: String?,
+                @SerializedName("phone")
+                val phone: String?,
+                @SerializedName("updated_at")
+                val updatedAt: String?,
+                @SerializedName("user_id")
+                val userId: String?
+            )
+        }
+
+        data class User(
+            @SerializedName("created_at")
+            val createdAt: String?,
+            @SerializedName("email")
+            val email: String?,
+            @SerializedName("email_verified_at")
+            val emailVerifiedAt: Any?,
+            @SerializedName("id")
+            val id: String?,
+            @SerializedName("role")
             val role: String?,
-            val updated_at: String?
+            @SerializedName("updated_at")
+            val updatedAt: String?
         )
     }
 }
--- app/src/main/java/com/apicta/myoscopealert/ui/screen/ProfileScreen.kt
+++ app/src/main/java/com/apicta/myoscopealert/ui/screen/ProfileScreen.kt
@@ -1,17 +1,25 @@
 package com.apicta.myoscopealert.ui.screen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
@@ -26,16 +34,48 @@ fun ProfileScreen(navController: NavHostController, storedToken: String) {
     val profileResponse by viewModel.profileResponse.collectAsState()
 
 
-    Surface(modifier = Modifier.fillMaxSize().padding(16.dp)) {
-        Column {
-            Text(text = "Profile Screen")
+    Surface(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)) {
+        Column(Modifier.fillMaxSize()) {
+            Text(text = "Profile Screen", fontWeight = FontWeight.Bold)
+            if (profileResponse == null) {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Center){
+                    CircularProgressIndicator()
+                }
+            } else {
 
-            Text(text = profileResponse?.data?.user?.id.toString())
-            Text(text = profileResponse?.data?.user?.email.toString())
+                Spacer(modifier = Modifier.height(32.dp))
+
+                Text(text = "Your Data", fontWeight = FontWeight.Bold)
+                Text(text = profileResponse?.data?.user?.id.toString())
+                Text(text = profileResponse?.data?.user?.email.toString())
+
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = profileResponse?.data?.profile?.fullname.toString())
+                Text(text = profileResponse?.data?.profile?.address.toString())
+                Text(text = profileResponse?.data?.profile?.phone.toString())
+                Text(text = profileResponse?.data?.profile?.emergencyPhone.toString())
+                Text(text = profileResponse?.data?.profile?.age.toString())
+                Text(text = profileResponse?.data?.profile?.gender.toString())
+                Text(text = profileResponse?.data?.profile?.condition.toString())
+
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = "Relations", fontWeight = FontWeight.Bold)
+                Text(text = profileResponse?.data?.profile?.relations?.get(0)?.fullname.toString())
+                Text(text = profileResponse?.data?.profile?.relations?.get(0)?.address.toString())
+                Text(text = profileResponse?.data?.profile?.relations?.get(0)?.phone.toString())
+                Text(text = profileResponse?.data?.profile?.relations?.get(0)?.emergencyPhone.toString())
+                Text(text = profileResponse?.data?.profile?.relations?.get(0)?.gender.toString())
+                Text(text = profileResponse?.data?.profile?.relations?.get(0)?.age.toString())
 
 
 
+                Spacer(modifier = Modifier.height(16.dp))
+
+            }
             Button(colors = ButtonDefaults.buttonColors(Color.Red),
+                modifier = Modifier.align(Alignment.CenterHorizontally),
                 onClick = {
                     viewModel.performLogout(storedToken)
                     navController.navigate("login_screen")
